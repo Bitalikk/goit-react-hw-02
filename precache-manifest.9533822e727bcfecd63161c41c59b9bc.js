e0edec859f1efa952e5bd9b1a337aa99bcdaa8bf
Vitalii Chenchovyi <orgvitaliy@gmail.com>
2019-05-27 16:08:24 +0300

    Updates

---
+++ precache-manifest.9533822e727bcfecd63161c41c59b9bc.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e77f5b2cccc57b54ab22c79187fbbbd3",
+    "url": "/goit-react-hw-02/index.html"
+  },
+  {
+    "revision": "3a57a9dd88100147f489",
+    "url": "/goit-react-hw-02/static/css/main.05dc4ab1.chunk.css"
+  },
+  {
+    "revision": "5c173b1cc72da319f620",
+    "url": "/goit-react-hw-02/static/js/2.f6e21d88.chunk.js"
+  },
+  {
+    "revision": "3a57a9dd88100147f489",
+    "url": "/goit-react-hw-02/static/js/main.93a1e592.chunk.js"
+  },
+  {
+    "revision": "d92f35f1efebc38103b8",
+    "url": "/goit-react-hw-02/static/js/runtime~main.577bbb3b.js"
+  }
+]);
