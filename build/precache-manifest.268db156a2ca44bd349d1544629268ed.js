27ed2c74768b1298748afecd7b1fa23f4c4215e2
Vitalii Chenchovyi <orgvitaliy@gmail.com>
2019-05-27 15:14:54 +0300

    React homework 02

---
+++ build/precache-manifest.268db156a2ca44bd349d1544629268ed.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f554b935fe32916379a4fdc6a180305b",
+    "url": "/index.html"
+  },
+  {
+    "revision": "3a57a9dd88100147f489",
+    "url": "/static/css/main.05dc4ab1.chunk.css"
+  },
+  {
+    "revision": "5c173b1cc72da319f620",
+    "url": "/static/js/2.f6e21d88.chunk.js"
+  },
+  {
+    "revision": "3a57a9dd88100147f489",
+    "url": "/static/js/main.93a1e592.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
